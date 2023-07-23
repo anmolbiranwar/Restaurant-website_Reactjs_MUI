0c957b61b26eaae7b73eac6ca1569f3239a9bf73
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-23 15:25:25 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Layout from "./components/Layout/Layout";
 
 
 function App() {
   return (
     <div className="App">
       <h1>Restaurant Website</h1>
+      <Layout />
     </div>
   );
 }
