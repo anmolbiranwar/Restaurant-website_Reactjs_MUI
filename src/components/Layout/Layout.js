5afa58a4676b642e8c2d4870e4c8a6b2e64d5680
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-23 15:25:34 -0700

    Update Layout.js

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+const Layout = () => {
+  return (
+    <>
+    <Header />
+        <h1>Layout</h1>
+    </>
+  )
+}
+
+export default Layout
