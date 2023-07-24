fe479ac0b47335236a35e573984e729a916f03cd
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-24 16:07:06 -0700

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {AppBar, Box, Toolbar, Typography} from "@mui/material"
 
 const Header = () => {
   return (
-    <div>
-       <h1>Navbar</h1> 
-    </div>
+    <>
+      <Box>
+        <AppBar component={"nav"} sx={{bgcolor:"black"}}>
+          <Toolbar>
+          <Typography>My Restaurant</Typography>
+          </Toolbar>
+        </AppBar>
+      </Box> 
+    </>
   )
 }
 
