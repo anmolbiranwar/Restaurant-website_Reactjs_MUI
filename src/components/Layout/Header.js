6b64f9950fe4e174947d840d4df61fb0679ba9d1
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-25 19:55:23 -0700

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -39,7 +39,9 @@ const Header = () => {
       <Box>
         <AppBar component={"nav"} sx={{bgcolor:"black"}}>
           <Toolbar>
-            <IconButton color='inherit' aria-label='open drawer' edge='start' sx={{mr:2, display:{sm:"none"},}}>
+            <IconButton color='inherit' aria-label='open drawer' edge='start' sx={{mr:2, display:{sm:"none"},}}
+            onClick={handleDrawerToggle} 
+            >
               <MenuIcon />
             </IconButton>
           <Typography color={"goldenrod"} variant='h6' component="div" sx={{flexGrow: 1}}>
