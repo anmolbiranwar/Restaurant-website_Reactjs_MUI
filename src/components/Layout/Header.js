1f97324b9552529fa0dce2266ee4d02e9c83cd81
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-25 19:49:14 -0700

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {AppBar, Box, IconButton, Toolbar, Typography} from "@mui/material";
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -6,12 +6,42 @@ import {Link} from 'react-router-dom';
 import "../../styles/HeaderStyles.css"
 
 const Header = () => {
+  const [mobileOpen, setMobileOpen]= useState(false)
+  //handle menu click
+  const handleDrawerToggle=()=>{
+    setMobileOpen(!mobileOpen)
+  }
+  //menu drawer
+  const drawer=(
+    <Box onClick={handleDrawerToggle} sx={{textAlign:'center'}}>
+           <Typography color={"goldenrod"} variant='h6' component="div" sx={{flexGrow: 1}}>
+            <FastfoodIcon/>
+            My Restaurant
+            </Typography>
+            <Box sx={{display: {xs:"none", sm:"block"}}}>
+              <ul className='navigation-menu'>
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/menu"}>Menu</Link>
+                </li>
+                <li>
+                  <Link to={"/about"}>About</Link>
+                </li>
+                <li>
+                  <Link to={"/contact"}>Contact</Link>
+                </li>
+              </ul>
+            </Box>
+    </Box>
+  )
   return (
     <>
       <Box>
         <AppBar component={"nav"} sx={{bgcolor:"black"}}>
           <Toolbar>
-            <IconButton color='inherit' aria-label='open drawer' edge='start' sx={{mr:2, display:{sm:"none"}}}>
+            <IconButton color='inherit' aria-label='open drawer' edge='start' sx={{mr:2, display:{sm:"none"},}}>
               <MenuIcon />
             </IconButton>
           <Typography color={"goldenrod"} variant='h6' component="div" sx={{flexGrow: 1}}>
