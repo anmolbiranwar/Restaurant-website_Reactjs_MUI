22323a047dbd2a55db70456b1204f9698b20f0e6
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-28 20:11:31 -0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -10,36 +10,36 @@ export const MenuList =[
         name:"Dosa",
         description:'A dosa is a thin savory pancake in South Indian cuisine made from a fermented batter of ground black lentils and rice.',
         image: Dosa,
-        price: 200
+        price: <span>&#8377;200</span>
     },
     {
         name:"Chola",
         description:'Chole Masala is a popular Punjabi dish where chickpeas are simmered in a spicy & tangy gravy.',
         image: Chola,
-        price: 250
+        price: <span>&#8377;220</span>
     },
     {
         name:"Idli",
         description:'Idli is a soft, pillowy steamed savory cake made from fermented rice and lentil batter.',
         image: Idli,
-        price: 300
+        price: <span>&#8377;180</span>
     },
     {
         name:"MasalaDosa",
         description:' Masala dosa are crispy rice and lentil crepes stuffed with spiced and savory potato filling.',
         image: MasalaDosa,
-        price: 350
+        price: <span>&#8377;250</span>
     },
     {
         name:"Paneer",
         description:'Paneer is an Indian cheese that made from curdled milk and some sort of fruit or vegetable acid like lemon juice',
         image: Paneer,
-        price: 350
+        price: <span>&#8377;200</span>
     },
     {
-        name:"Gujrati",
+        name:"Gujrati Thali",
         description:' Traditional Gujarati thali is a platter of various types of dishes from India state of Gujarat. ',
         image: Gujrati,
-        price: 300
+        price: <span>&#8377;350</span>
     }
 ]
