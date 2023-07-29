bc19f6c32c752a986095dc523f5f2d58ea05a28b
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-28 19:12:13 -0700

    Update Menu.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import {MenuList} from '../data/data'
+import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 
 const Menu = () => {
   return (
     <Layout>
-        <h1>Menu</h1>
+        <Box>
+          {
+            MenuList.map(menu =>(
+              <Card>
+                <CardActionArea>
+                  <CardMedia sx={{minHeight:'400px'}} component={'img'} src={menu.image} alt={menu.name} />
+                  <CardContent>
+                    <Typography variant='h5' gutterBottom component={'div'}>
+                      {menu.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))
+          }
+        </Box>
     </Layout>
   )
 }
