4a13bca40f15fa9ec8fc8dacfb7937734aedd95a
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-28 18:24:44 -0700

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout/Layout'
 import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 import EmailIcon from '@mui/icons-material/Email';
-
+import CallIcon from '@mui/icons-material/Call';
 
 const Contact = () => {
   return (
@@ -18,18 +18,23 @@ const Contact = () => {
             <Table aria-label='contact table'>
               <TableHead>
                 <TableRow>
-                  <TableCell>Contact Details</TableCell>
+                  <TableCell sx={{bgcolor:'black', color:'white'}} align='center'>Contact Details</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 <TableRow>
                   <TableCell>
-                    <SupportAgentIcon sx={{color:'red', pt:1}} /> 1234567890(tollfree)
+                    <SupportAgentIcon sx={{color:'red', pt:1}} /> 1800 00 7890(tollfree)
                   </TableCell>
                 </TableRow>
                 <TableRow>
                 <TableCell>
-                    <EmailIcon sx={{color:'skyblue', pt:1}} /> help@myrest.com
+                    <EmailIcon sx={{color:'blue', pt:1}} /> help@myrest.com
+                </TableCell>
+                </TableRow>
+                <TableRow>
+                <TableCell>
+                    <CallIcon sx={{color:'green', pt:1}} /> 1234567890
                 </TableCell>
                 </TableRow>
               </TableBody>
